c12644e56bad48f37f2604c05760c716cd45c384
Ezequiel <ezegonzalez800@gmail.com>
2024-05-29 18:32:12 -0300

    teoria.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,30 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+   {
+    path: 'login',
+    loadComponent: () => import('../../src/app/views/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('../../src/app/views/register/register.page').then(m => m.RegisterPage)
+  },
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'home/:id',
+    loadComponent: () => import('./views/user-detail/user-detail.page').then((m) => m.UserDetailPage),
+  },
+   {
+    path: 'ver-usuario/:id',
+    loadComponent: () => import('./views/ver-usuario/ver-usuario.component').then(m => m.VerUsuarioComponent)
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
 ];
